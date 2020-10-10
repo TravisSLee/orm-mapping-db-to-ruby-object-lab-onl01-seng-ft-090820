c1cbf70ae3a029f34b33245409b0abd7cc743e1e
Travis Lee <travisslee223@gmail.com>
2020-10-10 18:11:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -100,7 +100,7 @@ class Student
   
   def self.first_student_in_grade_10
     sql = <<-SQL
-      SELECT COUNT(*)
+      SELECT name
       FROM students
       WHERE students.grade = 10
       ORDER BY id
